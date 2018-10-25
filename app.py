44b975f94840640c94f78dacc07f435ca64bb5f0
tduval <duval.thomas01@gmail.com>
2018-10-25 03:02:19 -0700

    Update app.py API method

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import docker
 
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
 
 class Index(Resource):
     def get(self):
@@ -18,6 +20,7 @@ class Version(Resource):
     def get(self):
         return client.version()
 
+## Image Section ##
 class Images(Resource):
     def get(self):
         imagetable = []
@@ -25,6 +28,23 @@ class Images(Resource):
             imagetable.append(image.attrs)
         return imagetable
 
+    def post(self):
+        parser.add_argument('name', help='Package name')
+        parser.add_argument('tag', help='Package Tag/Version')
+        args = parser.parse_args()
+        if args.tag == "":
+            args.tag="latest"
+        img = client.images.pull(args.name, args.tag)
+        return img.attrs, 201
+
+class Image(Resource):
+    def get(self, image_id):
+        return client.images.get(image_id).attrs
+
+    def delete(self, image_id):
+        return client.images.remove(image_id), 204
+
+## Container Section ##
 class Containers(Resource):
     def get(self):
         containertable = []
@@ -33,13 +53,36 @@ class Containers(Resource):
         return containertable
 
 
+
+## Networks Section ##
+class Networks(Resource):
+    def get(self):
+        networktable = []
+        for network in client.networks.list():
+            networktable.append(network.attrs)
+        return networktable
+
+class Network(Resource):
+    def get(self, image_id):
+        return client.networks.get(net_id)
+
+    def delete(self, net_id):
+        return client.networks.remove(net_id), 204
+
+
+
 client = docker.from_env()
 api.add_resource(Index, '/')
 api.add_resource(Info, '/info')
 api.add_resource(Version, '/version')
+
 api.add_resource(Images, '/images')
+api.add_resource(Image, '/images/<image_id>')
+
 api.add_resource(Containers, '/containers')
 
+api.add_resource(Networks, '/networks')
+api.add_resource(Network, '/networks/<net_id>')
 
 if __name__ == '__main__':
     print("############ Docker docking status ############")
