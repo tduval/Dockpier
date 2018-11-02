4328337fea4aa2855f3900cdb77566139bd34303
tduval <duval.thomas01@gmail.com>
2018-11-02 10:54:33 +0800

    Setup base file for use with Vuetify UI Frameworks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
 <div id="app">
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/containers">Containers</router-link> |
-        <router-link to="/images">Images</router-link> |
-        <router-link to="/networks">Networks</router-link> |
-        <router-link to="/volumes">Volumes</router-link> |
-        <router-link to="/swarm">Swarm</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-app>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/containers">Containers</router-link> |
+            <router-link to="/images">Images</router-link> |
+            <router-link to="/networks">Networks</router-link> |
+            <router-link to="/volumes">Volumes</router-link> |
+            <router-link to="/swarm">Swarm</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
+    </v-app>
 </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import VueElementLoading from 'vue-element-loading';
 import App from './App.vue';
 import router from './router';
