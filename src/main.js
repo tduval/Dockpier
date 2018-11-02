ad69d18d8c985e2137773934d31de208e578c599
tduval <duval.thomas01@gmail.com>
2018-11-02 10:47:44 +0800

    Import Vuetify package as new UI Frameworks

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
+import Vuetify from 'vuetify';
 import VueElementLoading from 'vue-element-loading';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.use(VueMaterial);
+Vue.use(Vuetify);
 Vue.component('VueElementLoading', VueElementLoading);
 
 Vue.config.productionTip = false;
