a2e79161d1b403dfafd642a761bb19ef1d7c5fda
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-11-08 10:53:15 +0800

    Add new API for Images and Containers in python app.py

--- app.py
+++ app.py
@@ -54,6 +54,25 @@ class Image(Resource):
     def delete(self, image_id):
         return client.images.remove(image_id), 204
 
+class ImageHistory(Resource):
+    def get(self, image_id):
+        return client.images.get(image_id).history()
+
+class ImageTag(Resource):
+    def post(self, image_id):
+        parser.add_argument('repository', help='Repository name')
+        parser.add_argument('tag', help='tag name')
+        args = parser.parse_args()
+        img = client.images.get(image_id).tag(args.repository, args.tag)
+        return img, 201
+
+class ImageSearch(Resource):
+    def post(self):
+        parser.add_argument('name', help='Package name')
+        args = parser.parse_args()
+        img = client.images.serch(args.name)
+        return img, 201
+
 ## Container Section ##
 class Containers(Resource):
     def get(self):
@@ -62,7 +81,37 @@ class Containers(Resource):
             containertable.append(container.attrs)
         return containertable
 
+class Container(Resource):
+    def get(self, container_id):
+        return client.containers.get(container_id).attrs
 
+    def delete(self, container_id):
+        return client.containers.remove(container_id), 204
+
+class ContainerStatus(Resource):
+    def get(self, container_id):
+        return client.containers.get(container_id).status
+
+    def put(self):
+        parser.add_argument('status', help='actions type')
+        args = parser.parse_args()
+        container = client.container.get(container_id)
+        if args.status == "start":
+            return container.start(), 201
+        elif args.status == "restart":
+            return container.restart(), 201
+        elif args.status == "stop":
+            return container.stop(), 201
+        elif args.status == "pause":
+            return container.pause(), 201
+        elif args.status == "unpause":
+            return container.unpause(), 201
+        else:
+            return container, 400
+
+class ContainerLogs(Resource):
+    def get(self, container_id):
+        return client.containers.get(container_id).logs()
 
 ## Networks Section ##
 class Networks(Resource):
@@ -105,8 +154,14 @@ api.add_resource(Sys_Events, '/system/events')
 
 api.add_resource(Images, '/images')
 api.add_resource(Image, '/images/<image_id>')
+api.add_resource(ImageHistory, '/images/<image_id>/history')
+api.add_resource(ImageTag, '/images/<image_id>/tag')
+api.add_resource(ImageSearch, '/images/search')
 
 api.add_resource(Containers, '/containers')
+api.add_resource(Container, '/containers/<container_id>')
+api.add_resource(ContainerStatus, '/containers/<container_id>/status')
+api.add_resource(ContainerLogs, '/containers/<container_id>/logs')
 
 api.add_resource(Networks, '/networks')
 api.add_resource(Network, '/networks/<net_id>')
