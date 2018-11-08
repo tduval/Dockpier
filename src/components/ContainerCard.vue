bfc25133bc970e5a74b1047af3ddf205533b3049
Thomas Duval <Thomas.Duval@emeriocorp.com>
2018-11-08 16:01:22 +0800

    Add tooltip on top of speed dial button for containers acitons

--- src/components/ContainerCard.vue
+++ src/components/ContainerCard.vue
@@ -3,38 +3,53 @@
     <v-hover>
         <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`">
             <v-card-title primary-title>
-              <div>
-                  <v-speed-dial absolute right direction="left" v-model="fab">
-                      <v-btn fab dark small slot="activator" v-model="fab">
-                        <v-icon>menu</v-icon>
-                        <v-icon>close</v-icon>
-                      </v-btn>
-                      <v-btn fab dark small color="warning"
-                             v-if='cntr.State.Running && !cntr.State.Paused'>
-                          <!-- Pause -->
-                          <v-icon>pause</v-icon>
-                      </v-btn>
-                      <v-btn fab small
-                             v-if='cntr.State.Paused'>
-                          <!-- Unpause -->
-                          <v-icon>pause</v-icon>
-                      </v-btn>
-                      <v-btn fab dark small color="error"
-                             v-if='cntr.State.Running && !cntr.State.Paused'>
-                          <!-- Stop -->
-                          <v-icon>stop</v-icon>
-                      </v-btn>
-                      <v-btn fab dark small color="success"
-                             v-if='!cntr.State.Running'>
-                          <!-- Start -->
-                          <v-icon>play_arrow</v-icon>
-                      </v-btn>
-                      <v-btn fab dark small color="primary"
-                             v-if='cntr.State.Running || cntr.State.Paused'>
-                          <!-- Restart -->
-                          <v-icon>replay</v-icon>
-                      </v-btn>
-                  </v-speed-dial>
+                <div>
+                    <v-speed-dial absolute right direction="left" v-model="fab">
+                        <v-btn fab dark small slot="activator" v-model="fab">
+                            <v-icon>menu</v-icon>
+                            <v-icon>close</v-icon>
+                        </v-btn>
+                        <v-tooltip top color="warning" transition="scale-transition">
+                            <v-btn fab dark small color="warning"
+                                 v-if='cntr.State.Running && !cntr.State.Paused'
+                                 slot="activator">
+                              <v-icon>pause</v-icon>
+                            </v-btn>
+                            <span>Pause</span>
+                        </v-tooltip>
+                        <v-tooltip top transition="scale-transition">
+                            <v-btn fab small
+                                v-if='cntr.State.Paused'
+                                slot="activator">
+                                <v-icon>pause</v-icon>
+                            </v-btn>
+                            <span>Unpause</span>
+                        </v-tooltip>
+                        <v-tooltip top color="error" transition="scale-transition">
+                            <v-btn fab dark small color="error"
+                                 v-if='cntr.State.Running && !cntr.State.Paused'
+                                 slot="activator">
+                                <v-icon>stop</v-icon>
+                            </v-btn>
+                            <span>Stop</span>
+                        </v-tooltip>
+                        <v-tooltip top color="success" transition="scale-transition">
+                            <v-btn fab dark small color="success"
+                                 v-if='!cntr.State.Running'
+                                 slot="activator">
+                                <v-icon>play_arrow</v-icon>
+                            </v-btn>
+                            <span>Start</span>
+                        </v-tooltip>
+                        <v-tooltip top color="primary" transition="scale-transition">
+                            <v-btn fab dark small color="primary"
+                                 v-if='cntr.State.Running || cntr.State.Paused'
+                                 slot="activator">
+                                <v-icon>replay</v-icon>
+                            </v-btn>
+                            <span>Restart</span>
+                        </v-tooltip>
+                    </v-speed-dial>
                 <div class="headline">
                     <v-tooltip top>
                         <v-avatar color='warning' slot="activator" size="26px"
@@ -59,7 +74,7 @@
                 <span class="grey--text">Run CMD :
                     <strong>{{ cntr.Config.Cmd }}</strong>
                 </span>
-              </div>
+                </div>
             </v-card-title>
 
             <v-card-text>
