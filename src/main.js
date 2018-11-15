43423d0295502d3fd14d3c9e5ec86a3bcb37f0d9
tduval <duval.thomas01@gmail.com>
2018-11-15 17:25:19 +0800

    Install vue-numeral filter package

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import VueElementLoading from 'vue-element-loading';
 import Vue2Filters from 'vue2-filters';
+import vueNumeralFilterInstaller from 'vue-numeral-filter';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -10,6 +11,7 @@ import store from './store';
 Vue.use(Vuetify);
 Vue.component('VueElementLoading', VueElementLoading);
 Vue.use(Vue2Filters);
+Vue.use(vueNumeralFilterInstaller);
 Vue.use(require('vue-moment-jalaali'));
 
 Vue.config.productionTip = false;
